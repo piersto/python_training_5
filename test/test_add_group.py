86ffa84b79a64fad3038aa716180126c254e66df
PStoiko <pstoiko@gmail.com>
2019-01-02 17:17:24 -0500

    Параметризация тестов. Data Driven Testing. Added random choice to test_add_group.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
 
-def test_add_group(app):
+def random_string(prefix, maxlen):
+    # string will be chosen from letters, digits and 10 spaces -- ' '*10
+    symbols = string.ascii_letters + string.digits + ' '*5
+    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata=[
+    # will generate random string that starts with word 'Name' or 'Header etc and + some more random symbols
+    Group(name=random_string('Name', (10)), header=random_string('Header', (5)), footer=random_string('footer', (7)))
+    ]
+
+@pytest.mark.parametrize('group', testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name='Name group', header='Group header', footer='Group footer')
     app.group.create_group(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
