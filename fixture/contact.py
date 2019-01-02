6386cda62d8ce5707b7bba0e11966af1b412c8fe
Pierre <pstoiko@gmail.com>
2019-01-01 21:24:21 -0500

    Режем строки на части: делаем тесты для проверки телефонов. Finished 10 min. Test failed

--- fixture/contact.py
+++ fixture/contact.py
@@ -120,8 +120,26 @@ class ContactHelper:
                 firstname = cells[2].text
                 lastname = cells[1].text
                 id = cells[0].find_element_by_name("selected[]").get_attribute('value')
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id))
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(firstname=firstname,
+                                                  lastname=lastname,
+                                                  id=id,
+                                                  homephone=all_phones[0],
+                                                  mobilephone=all_phones[1],
+                                                  workphone=all_phones[2],
+                                                  secondaryphone=all_phones[3]))
         return list(self.contact_cache)
 
     def get_contact_info_from_edit_page(self, index):
-        self.modify_contact_by_index(index)
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        firstname = wd.find_element_by_name('firstname').get_attribute('value')
+        lastname = wd.find_element_by_name('lastname').get_attribute('value')
+        id = wd.find_element_by_name('id').get_attribute('value')
+        homephone = wd.find_element_by_name('home').get_attribute('value')
+        mobilephone = wd.find_element_by_name('mobile').get_attribute('value')
+        workphone = wd.find_element_by_name('work').get_attribute('value')
+        secondaryphone = wd.find_element_by_name('phone2').get_attribute('value')
+        return Contact(firstname=firstname, lastname=lastname, id=id, homephone=homephone, mobilephone=mobilephone,
+                       workphone=workphone, secondaryphone=secondaryphone)
+
